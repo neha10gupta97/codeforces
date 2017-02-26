41d1f1058cb8a585d9ac2b60e5b5225220df96ff
Neha <neha10gupta97@gmail.com>
2017-02-27 03:54:39 +0530

    82A.cpp

--- 82A.cpp
+++ 82A.cpp
@@ -3,4 +3,11 @@ using namespace std;
 main(){
 	int n;
 	cin>>n;
-	
+	int r=1;
+	while(r*5<n){
+		n-=r*5;
+		r*=2;
+	}
+	string name[]={"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+	cout<<name[(n-1)/r]<<endl;
+}
